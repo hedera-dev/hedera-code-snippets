c09fd6832573fc87f53d49e98d51d42383f29ebf
Abi Castro <abixcastro@gmail.com>
2023-08-29 15:36:26 -0700

    code snippet to associate an account with an HTS token

---
+++ associate-token-via-ethers/README.md
@@ -0,0 +1,18 @@
+# Associate Token via Ethers
+
+Associate an account with the provided HTS token via ethers.js.
+
+
+Associating an account with a Hedera Native token is unique to Hedera and is required for someone to send an HTS token to a wallet. Needing to associate protects a wallet holder from recieving unwanted tokens.
+
+You are able to treat HTS tokens as if they were ERC20/ERC721 tokens. This allows us to call the associate function directly on the HTS token.
+
+## Code
+The file `index.js` provides a code snippet to associate an account with a Hedera native token via ethers.js. 
+
+### How to use
+Add this code snippet into an already existing project that establishes a connection to an evm wallet (i.e. MetaMask). The wallet user will need to sign the transaction and if successful, their account is associated with the token.
+
+## References
+
+- [HIP-719: Associate and Dissociate Tokens via Facade Contract](https://hips.hedera.com/hip/hip-719)
---
+++ associate-token-via-ethers/index.js
@@ -0,0 +1,31 @@
+import ethers from 'ethers';
+import { TokenId,} from '@hashgraph/sdk';
+
+/* Associate an account with the provided HTS token id by 
+  treating HTS tokens as if they were ERC20/ERC721 tokens. 
+  This allows us to call the associate functino directly on the HTS token.
+*/
+async function associateToken() {
+  // set up your ethers provider
+  const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+  // request access to the user's account
+  await provider.send("eth_requestAccounts", []);
+  // get signer
+  const signer = provider.getSigner();
+  // define contract interface
+  const abi = ["function associate()"];
+  const hederaTokenID = '0.0.572609';
+  // when working with HTS token Ids, convert the HTS tokenID into it’s solidity address with a ‘0x’ prefix
+  const tokenSolidityAddress = '0x' + TokenId.fromString(hederaTokenID).tokenSolidityAddress();
+  // create contract instance using token solidity address, abi, and signer
+  const contract = new ethers.Contract(tokenSolidityAddress, abi, signer);
+
+  try {
+    // call the associate function
+    const transactionResult = await contract.associate();
+    return transactionResult.hash;
+  } catch (error) {
+    console.warn(error.message ? error.message : error);
+    return null;
+  } 
+};
