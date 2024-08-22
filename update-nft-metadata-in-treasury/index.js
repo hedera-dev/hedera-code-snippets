7545d249d44b3b533f1d36a350a692ee63f3a627
Abi Woo-Sam <abixcastro@gmail.com>
2024-08-22 10:47:18 -0700

    update output to show metadat before update and  metadata after update

--- update-nft-metadata-in-treasury/index.js
+++ update-nft-metadata-in-treasury/index.js
@@ -29,17 +29,19 @@ async function createNftCollection() {
 
     // This is the nft collection Id
     const tokenId = (await tokenReceipt).tokenId;
-    console.log("The NFT collection ID is " + tokenId);
+    console.log(`The NFT collection ID is ${tokenId}`);
 
     return tokenId;
 }
 
 async function mintTokenWithMetadata(tokenId) {
-    // Metadata to mint the NFT with
-    // Note: You need to store your token metadata using a storage service, 
-    // either centralized or decentralized, like IPFS or Arweave, because the token metadata JSON schema V2 requires it.
-    const metadata = new Uint8Array([1]);
-
+    /*
+        Metadata should follow the JSON schema V2. 
+        This schema requires you to store your token metadata using a storage service.
+        View HIP-412 to learn more https://hips.hedera.com/hip/hip-412
+    */
+    const metadata = new Uint8Array([1]); // This should be a URI string [protocol://resource_location] or CID
+    
     // Mint an NFT and set the metadata
     const mintNft = new TokenMintTransaction()
         .setMetadata(metadata)
@@ -51,7 +53,8 @@ async function mintTokenWithMetadata(tokenId) {
 
     const tokenMintReceipt = (await submitTx2).getReceipt(client);
     const nftSerial = (await tokenMintReceipt).serials;
-    console.log("The NFT serial number is " + nftSerial);
+    console.log(`The NFT serial number is ${nftSerial}`);
+    console.log(`The NFT metadata is: ${metadata}`);
 
     return nftSerial;
 }
@@ -80,6 +83,7 @@ async function updateNftMetadataWithinTreasury(tokenId, nftSerial) {
     const getReceipt = (await submitTx).getReceipt(client);
     const status = (await getReceipt).status;
     console.log("The NFT metadata update transaction was " + status.toString());
+    console.log(`The NFT meta data is: ${newMetadata}`);
 }
 
 const nftCollectionID = await createNftCollection();
