78fbef57ebec87b624c4ef59b1dd1a2fbe6de54e
Michiel Mulders <michielmulders1@gmail.com>
2025-05-02 19:00:13 +0200

    Update UUPS contract
    Signed-off-by: Michiel Mulders <michielmulders1@gmail.com>

--- hardhat-erc-721-mint-burn/contracts/erc-721-upgrade.sol
+++ hardhat-erc-721-mint-burn/contracts/erc-721-upgrade.sol
@@ -5,8 +5,9 @@ pragma solidity ^0.8.22;
 import {ERC721Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
-contract MyTokenUpgradeable is Initializable, ERC721Upgradeable, OwnableUpgradeable {
+contract MyTokenUpgradeable is Initializable, ERC721Upgradeable, OwnableUpgradeable, UUPSUpgradeable {
     uint256 private _nextTokenId;
 
     /// @custom:oz-upgrades-unsafe-allow constructor
@@ -17,6 +18,7 @@ contract MyTokenUpgradeable is Initializable, ERC721Upgradeable, OwnableUpgradea
     function initialize(address initialOwner) public initializer {
         __ERC721_init("MyTokenUpgradeable", "MTU");
         __Ownable_init(initialOwner);
+        __UUPSUpgradeable_init();
     }
 
     function safeMint(address to) public onlyOwner returns (uint256) {
@@ -24,4 +26,10 @@ contract MyTokenUpgradeable is Initializable, ERC721Upgradeable, OwnableUpgradea
         _safeMint(to, tokenId);
         return tokenId;
     }
+
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        override
+        onlyOwner
+    {}
 }
